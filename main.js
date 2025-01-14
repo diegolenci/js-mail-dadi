94d105324d6e6a7ad46c6214fd77e6ac195458a7
diegolenci <lencidiego0@gmail.com>
2025-01-14 17:16:09 +0100

    Esercizio dadi completato

---
+++ main.js
@@ -0,0 +1,48 @@
+/*Mail
+Crea una lista di email di invitati ad una festa.
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo for.
+Non è consentito usare nessun metodo proprio degli array (come includes, per esempio).
+Si può fare? Certo che si basta ragionare un po’.
+Nota:
+Non è necessario provvedere alla validazione delle email
+
+
+Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.*/
+
+//ESERCIZIO 1
+//elenco mail
+/*const email = [
+    paperinogmail.com,
+    pippogmail.com,
+    mariorossigmail.com,
+    topolinogmail.com
+]
+
+//richiesta mail a utente
+
+let richiesta = prompt ("inserisci la tua mail")*/
+
+
+
+
+
+//ESERCIZIO 2
+//variabili con numeri random
+let number1 = Math.floor(Math.random() * 6)
+let number2 = Math.floor(Math.random() * 6)
+
+console.log (number1 , number2)
+
+//condizioni
+if ( number1 > number2){
+    console.log (`${number1} ha vinto il giocatore`)
+} else if ( number1 < number2){
+    console.log (`${number2} ha vinto il computer`)
+} else {
+    console.log (`${number1} ${number2} pareggio`)
+}
+
